9bb909b5c23f1e0959c62190b748ada33e3ac1fc
JAldo1980 <jamesalderman1980@gmail.com>
2023-12-22 09:57:06 +0100

    first commit

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
-const dangerArray = [
-  ["🎅", "👺"],
-  [
-    ["🎅", "🦁"],
-    ["👹", "🎅"],
-  ],
-  [
-    [
-      ["🎅", "🐻"],
-      ["🧌", "🎅"],
-    ],
-    [
-      ["🐯", "🎅"],
-      ["🎅", "😈"],
-    ],
-  ],
-];
+let people = [];
 
-const output = document.querySelector(".output");
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const peopleListEl = document.getElementById("people-list");
 
-// First, I will need to create a DOM element to contain and display any 'santa's' that are found...
-function displaySantaOnDOM(element) {
-  const div = document.createElement("div");
-  div.innerText = element;
-  div.className = "santa-element";
-  output.appendChild(div);
-}
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
 
-// I've not done recursion before - at first it was hard to get my head round it...here's what I found out:
-// 1. create a for of loop to iterate over the array...
-function findAndDisplaySanta(array) {
-  for (const element of array) {
-    //   This line below checks if the current element being iterated is itself an array...(I had no idea this could be done!)
-    if (Array.isArray(element)) {
-      // below ...this hurt my head a little...so, if the current element is an array, the function calls itself (this is called recursively)
-      findAndDisplaySanta(element);
-      //   to end, if the current element is not an array, the function checks if it is equal to the string "🎅".
-    } else if (element === "🎅") {
-      // if so, the target / element is displayed on the DOM...calling the function I created earlier...
-      displaySantaOnDOM(element);
-    }
+  if (inputValue) {
+    people.push(inputValue);
+
+    clearInputFieldEl();
+
+    renderList(people);
+  }
+});
+
+function renderList(array) {
+  clearPeopleListEl();
+
+  for (let i = 0; i < array.length; i++) {
+    let currentPerson = array[i];
+
+    appendPersonToPeopleListEl(currentPerson);
   }
 }
 
-findAndDisplaySanta(dangerArray);
+function clearPeopleListEl() {
+  peopleListEl.innerHTML = "";
+}
+
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
+}
+
+function appendPersonToPeopleListEl(person) {
+  let newEl = document.createElement("li");
+
+  newEl.textContent = person;
+
+  newEl.addEventListener("dblclick", function () {
+    let index = people.indexOf(person);
+
+    people.splice(index, 1);
+
+    renderList(people);
+  });
+
+  peopleListEl.append(newEl);
+}
